8801ff0115f762a858bc5276570b7806a0d0e697
Imran Khan <Imran@Imrans-MBP.home>
2015-12-16 23:13:24 +0000

    Added Pokemon class

---
+++ Pokedex/pokemon.swift
@@ -0,0 +1,34 @@
+//
+//  pokemon.swift
+//  Pokedex
+//
+//  Created by Imran Khan on 16/12/2015.
+//  Copyright © 2015 Imran Khan. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId : Int!
+    
+    
+    var name : String {
+        return _name
+    }
+    
+    var pokedexId : Int {
+        return _pokedexId
+    }
+    
+    
+    init(name: String, pokedexId: Int){
+    
+    self._name = name
+    self._pokedexId  = pokedexId
+    
+    }
+    
+    
+}
